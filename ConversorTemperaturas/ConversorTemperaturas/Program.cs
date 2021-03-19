332b19a5a20eb65b4a01296fcb1803e7c438cb98
Wagner Lima <wagner.araujo92@outlook.com>
2021-03-19 20:56:43 -0300

    Conversor de Temperaturas

---
+++ ConversorTemperaturas/ConversorTemperaturas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperaturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f, k;
+
+            Console.WriteLine("### CONVERSOR DE TEMPERATURAS ###");
+            Console.Write("Insira a temperatura em Celcius: ");
+            c = double.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------------");
+
+            // (c * 9 / 5) + 32 = F
+            f = (c * 9 / 5) + 32;
+
+            // c + 273,15
+            k = c + 273.15;
+
+            Console.WriteLine(c + " graus celcius = " + f + " graus fahrenheit");
+            Console.WriteLine(c + " graus celcius = " + k + " graus kelvin");
+            Console.WriteLine("---------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
