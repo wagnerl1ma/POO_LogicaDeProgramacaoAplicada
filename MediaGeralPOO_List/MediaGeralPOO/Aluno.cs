67a2b83304bfb0de8b12a7d8b483d19bfe796406
Wagner Lima <wagner.araujo92@outlook.com>
2021-04-02 18:08:05 -0300

    Calculos de Média dos Alunos com Listas ao invés de Arrays

---
+++ MediaGeralPOO_List/MediaGeralPOO/Aluno.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO_List
+{
+    class Aluno
+    {
+        public string Nome { get; private set; }
+
+        private List<double> Notas { get; set; } = new List<double>();
+
+        public int QtdProva { get; set; }
+
+        public double Media { get { return CalcularMedia(); } }
+
+        public Aluno(string nome, int qtdProvas)
+        {
+            Nome = nome;
+            QtdProva = qtdProvas;
+            //Notas = new List<double> { 0 };
+            //var resultado = Notas[0];
+        }
+
+        public void InserirNotas()
+        {
+
+            for (int i = 0; i < QtdProva; i++)
+            {
+
+                Console.Write("Nota #" + (i + 1) + ": ");
+                Notas.Add(double.Parse(Console.ReadLine()));
+
+                //ou
+                //Notas[i] = double.Parse(Console.ReadLine());
+                //Notas.Add(i);
+            }
+
+        }
+
+
+
+        private double CalcularMedia()
+        {
+            double total = 0;
+
+            for (int i = 0; i < QtdProva; i++)
+            {
+                total += Notas[i];
+            }
+
+            return total / Notas.Count;
+        }
+    }
+}
---
+++ MediaGeralPOO_List/MediaGeralPOO/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "### Média Geral dos Alunos ###";
+
+            Console.Write("Quantos alunos? ");
+            int nAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            List<Aluno> alunos = new List<Aluno>();
+
+            for (int i = 0; i < nAlunos; i++)
+            {
+                Console.Clear();
+                Console.Write("Aluno #" + (i + 1) + " Nome..: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Aluno #" + (i + 1) + " Provas: ");
+                int provas = int.Parse(Console.ReadLine());
+
+                alunos.Add(new Aluno(nome, provas));
+                //alunos[i] = new Aluno(nome, provas);
+
+                Console.WriteLine("Insira as notas do aluno: " + nome);
+                alunos[i].InserirNotas();
+
+            }
+
+
+            Console.Clear();
+
+            double mediaGeral = 0;
+
+            foreach (Aluno aluno in alunos)
+            {
+                Console.WriteLine("Aluno: " + aluno.Nome);
+                Console.WriteLine("Media: " + aluno.Media);
+                Console.WriteLine();
+
+                mediaGeral += aluno.Media;
+
+            }
+
+            double resultadoFinal = mediaGeral / alunos.Count;
+
+            Console.WriteLine("Média geral dos alunos: " + resultadoFinal);
+
+            Console.ReadKey();
+        }
+    }
+}
